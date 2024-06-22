840fdcc59d56d128cf1a3d3a9456c115c26499dd
lama <aloatibilama196.com>
2024-06-22 21:29:17 +0300

    centered white rectangle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>المعلومات الشخصية</h1>
+<hr>
+
+<p>
+     لمى 
+</p>
+
+
+<p>
+    مبرمجة
+</p>
+
+<p>
+    123456789
+</p>
+
+<h1>السيرة الذاتية</h1>
+<hr>
+<p>
+    Lama, a junior programmer with one year of experience, proficient in Python and Java.
+</p>
+<hr>
+<h1>المشاريع</h1>
+
+<p>
+    Developed several projects, including a daily task tracking application using Python, an e-commerce website in Java, a library management system in Java and Swing, a language learning application using Python and the Cute UI, in addition to an appointment booking system using Java and Springbot.
+</p>
+
+
