b34ce6a57d413873f93262270ae148fa2950b553
lama <aloatibilama196.com>
2024-06-23 19:04:26 +0300

    shadows

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,26 @@
-<div class="bg-white  rounded-3xl grid grid-cols-4 gap-4 ">
-<button> % </button>
-<button> √ </button>
-<button> CE </button>
-<button> C </button>
+<div class="bg-white  rounded-3xl grid grid-cols-4 gap-2 p-14 pt-15 font-normal shadow-2xl">
+<div class="bg-slate-500 text-white rounded-full col-span-4 h-12 w-30 flex items-center px-5 mb-5 font-bold">
+2
+</div>
+
+<button class="bg-blue-500"> % </button>
+<button class="bg-yellow-400"> √ </button>
+<button class="bg-red-500 "> CE </button>
+<button class=" bg-orange-400 text-2xl"> C </button>
 <button> 7 </button>
 <button> 8 </button>
 <button> 9 </button>
-<button> - </button>
+<button class="bg-green-700 text-4xl"> - </button>
 <button> 4 </button>
 <button> 5 </button>
 <button> 6 </button>
-<button> ÷ </button>
+<button class="bg-sky-500 text-3xl"> ÷ </button>
 <button> 1 </button>
 <button> 2 </button>
 <button> 3 </button>
-<button> × </button>
+<button class="bg-pink-600 text-3xl"> × </button>
 <button> . </button>
 <button> 0 </button>
 <button> =  </button>
-<button> + </button>
+<button class="bg-violet-500 text-3xl"> + </button>
 </div>
